b5e2bcfca78f4f6dc3a1a7f47ccf1676c4de68c3
michaelcmarafino <michaelcmarafino@gmail.com>
2023-03-14 11:28:11 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+import { StyleSheet, View, FlatList, Button } from "react-native"
+import GoalItem from "./components/GoalItem"
+import GoalInput from "./components/GoalInput"
+import { StatusBar } from "expo-status-bar"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [courseGoals, setCourseGoals] = useState([])
+    const [isModalVisible, setIsModalVisible] = useState(false)
+
+    function startAddGoalHandler() {
+        setIsModalVisible(true)
+    }
+
+    function endAddGoalHandler() {
+        setIsModalVisible(false)
+    }
+
+    function addGoalHandler(enteredGoalText) {
+        setCourseGoals((prev) => [
+            ...prev,
+            { text: enteredGoalText, id: Math.random().toString() },
+        ])
+        endAddGoalHandler()
+    }
+
+    function deleteGoalHandler(id) {
+        setCourseGoals((prev) => prev.filter((goal) => goal.id !== id))
+    }
+
+    return (
+        <>
+            <StatusBar style="light" />
+            <View style={styles.appContainer}>
+                <GoalInput
+                    onAddGoal={addGoalHandler}
+                    visible={isModalVisible}
+                    onCancel={endAddGoalHandler}
+                />
+                <View style={styles.goalsContainer}>
+                    <FlatList
+                        data={courseGoals}
+                        renderItem={(itemData) => {
+                            return (
+                                <GoalItem
+                                    text={itemData.item.text}
+                                    onDeleteItem={deleteGoalHandler}
+                                    id={itemData.item.id}
+                                />
+                            )
+                        }}
+                        keyExtractor={(item, index) => {
+                            return item.id
+                        }}
+                        alwaysBounceVertical={false}
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        title="Add New Goal"
+                        color="#b180f0"
+                        onPress={startAddGoalHandler}
+                    />
+                </View>
+            </View>
+        </>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    appContainer: {
+        paddingTop: 50,
+        paddingHorizontal: 16,
+        flex: 1,
+    },
+
+    goalsContainer: {
+        flex: 5,
+    },
+    buttonContainer: {
+        marginBottom: 40,
+    },
+})
---
+++ README.md
@@ -0,0 +1,22 @@
+# React Native Prototype
+
+## Installation for development:
+
+Clone the repository into any directory.
+
+```bash
+git clone https://github.com/michaelcmarafino/knews-app.git
+
+```
+
+Start a terminal in the directory of the step from above, and run the following command:
+
+```bash
+npm install
+```
+
+In the same directory, start the local server by running the following:
+
+```bash
+npm start
+```
